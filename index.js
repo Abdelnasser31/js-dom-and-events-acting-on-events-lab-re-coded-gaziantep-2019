8c9954274f026357dedf6d465745997b0b562055
Abdelnasser Alkhellow <abdelnasser.kh10@gmail.com>
2019-09-13 09:05:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 function preventRefreshOnSubmit(){
     document.querySelector('form').addEventListener('submit', function(event){
-      event.preventDefault()
+      event.preventDefault();
+      addNewElementAsLi();
     })
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation() {
+ return document.querySelector('form input').value
+}
+function addNewElementAsLi() {
+  let node = document.createElement("LI");    
+  node.innerHTML = retrieveEmployeeInformation();
+   document.querySelector('ul.employee-list').appendChild(node);
+}
+function addNewLiOnClick() {
+    let node = document.createElement("LI");    
+  node.innerHTML = retrieveEmployeeInformation();
+   document.querySelector('ul.employee-list').appendChild(node);
+}
+function clearEmployeeListOnLinkClick() {
+  document.querySelector('a').addEventListener('click', function() {
+    document.querySelector('ul').innerHTML = "";
+  })
+}
